2eb589da2abccc40f2c0a2fc13179518d34610af
genka <tamani.coding@gmail.com>
2021-05-08 12:18:26 +0200

    background color

--- src/renderer.ts
+++ src/renderer.ts
@@ -48,7 +48,7 @@ export class WebGpuRenderer {
                 {
                     // attachment is acquired and set in render loop.
                     view: undefined,
-                    loadValue: { r: 0.5, g: 0.5, b: 0.5, a: 1.0 },
+                    loadValue: { r: 0.7, g: 0.7, b: 0.7, a: 1.0 },
                 } as GPURenderPassColorAttachmentNew,
             ],
             depthStencilAttachment: {
