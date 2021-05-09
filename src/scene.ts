3f77f193fa40c26541fe62b252f8c56e4e6e868e
genka <tamani.coding@gmail.com>
2021-05-09 14:53:21 +0200

    refactore

--- src/scene.ts
+++ src/scene.ts
@@ -1,7 +1,7 @@
-import { mat4, vec3 } from 'gl-matrix';
+import { vec3 } from 'gl-matrix';
 import { Cube } from './objects';
 
-export const lightDataSize = 3 * 4; // 4x4 matrix size + vec3 size
+export const lightDataSize = 3 * 4; // vec3 size in bytes
 
 export class Scene {
 
